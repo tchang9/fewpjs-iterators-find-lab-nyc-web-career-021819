f4f5bf01e37b77d0526c5c6114774c64480925df
tchang9 <tlchang@gmail.com>
2019-04-11 16:48:25 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+  const firstWin = array.find(game => game.result === "W")
+  return (firstWin ? firstWin.year : undefined)
+}
